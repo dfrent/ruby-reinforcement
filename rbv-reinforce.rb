b541e69df582c52126fa5b35b3a1658adf71254a
Your Name <you@example.com>
2017-11-01 21:54:20 -0400

    rbv reinforcement exercise 1

--- rbv-reinforce.rb
+++ rbv-reinforce.rb
@@ -1,16 +1,25 @@
-drama = som
-comedy = ted
-dramady = bucket_list
-documentary = earth 
 
-puts "would you like to enjoy a movie? 1. documentary 2.comedy 3.drama 4.dramady"
-  answer == gets.chomp
-if answer == documentary
+drama = "moonlight"
+comedy = "ted"
+dramady = "bucket_list"
+documentary = "earth" 
+
+puts "do you enjoy documentaries"
+  answer1 = gets.chomp
+  puts "do you enjoy comedies"
+  answer2 = gets.chomp
+puts "do you enjoy a dramas"
+  answer3 = gets.chomp
+ 
+if answer1 == "yes"
 	puts "#{documentary}"
-elsif 
-elsif 
-elsif
-elsif
-else
-	
-end
+elsif answer1 == "no" && answer2 == "yes" && answer3 == "yes"
+		puts "#{dramady}"
+elsif answer3 == "yes"
+	puts "#{drama}"
+elsif answer2 == "yes"
+	puts "#{comedy}"
+else answer1 == "no" && answer2 == "no" && answer3 == "no"
+	puts "try a good book -think and grow rich"
+ end
+
