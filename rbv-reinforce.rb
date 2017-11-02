c301cb19d41fc73baaa6c6e3d269d4ee8d4c7f1f
Your Name <you@example.com>
2017-11-01 21:04:05 -0400

    ruby reinforcement exercise intial

---
+++ rbv-reinforce.rb
@@ -0,0 +1,16 @@
+drama = som
+comedy = ted
+dramady = bucket_list
+documentary = earth 
+
+puts "would you like to enjoy a movie? 1. documentary 2.comedy 3.drama 4.dramady"
+  answer == gets.chomp
+if answer == documentary
+	puts "#{documentary}"
+elsif 
+elsif 
+elsif
+elsif
+else
+	
+end
